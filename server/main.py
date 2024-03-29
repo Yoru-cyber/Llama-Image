3a2591bf402ee7fa4fc7423971224999a1b1fd69
Yoru-cyber <carlosmendez170210@gmail.com>
2024-03-29 18:57:49 -0400

    changed response to return json with path of created file

--- server/main.py
+++ server/main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from PIL import Image
 import random
+
 app = FastAPI()
 
-origins =[
-        "http://localhost:5000",
+origins = [
+    "http://localhost:5000",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -16,20 +17,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def read_root():
-    return {"Hello": "You are in the root. If you read this everything works alright. Go to /docs for documentation"}
+    return {
+        "Hello": "You are in the root. If you read this everything works alright. Go to /docs for documentation"
+    }
 
 
-#http://localhost:8000/convert?type=png
+# http://localhost:8000/convert?type=png
 @app.post("/convert")
 async def read_image(user_image: UploadFile, type: str):
     if user_image and type:
         randomSeed = random.seed()
-        hashCode = hash(f'{user_image.filename}{randomSeed}')
+        hashCode = hash(f"{user_image.filename}{randomSeed}")
         image = Image.open(user_image.file)
-        image.save(f'./images/{hashCode}.{type}') #to avoid having the possibility of a file with the same name, add a hash code with a random seeded value to the filename
-        return FileResponse(f'./images/{hashCode}.{type}')
+        image.save(
+            f"./images/{hashCode}.{type}"
+        )  # to avoid having the possibility of a file with the same name, add a hash code with a random seeded value to the filename
+        path = f"./images/{hashCode}.png"
+        return {"status_code": 201, "message": "Object created", "path": path}
 
 
 # http://localhost:8000/scale?scaling=2
@@ -37,12 +44,15 @@ async def read_image(user_image: UploadFile, type: str):
 async def scale_image(scaling: int, user_image: UploadFile):
     if user_image and scaling:
         randomSeed = random.seed()
-        hashCode = hash(f'{user_image.filename}{randomSeed}')
+        hashCode = hash(f"{user_image.filename}{randomSeed}")
         image = Image.open(user_image.file)
         image = image.resize([image.width * scaling, image.height * scaling])
-        image.save(f'./images/{hashCode}.png')
-        return FileResponse(f'./images/{hashCode}.png')
+        image.save(f"./images/{hashCode}.png")
+        path = f"./images/{hashCode}.png"
+        return {"status_code": 201, "message": "Object created", "path": path}
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
