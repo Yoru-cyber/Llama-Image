2716fffbafe9c17680d84718eea61689328cf033
Yoru-cyber <carlosmendez170210@gmail.com>
2024-04-01 05:09:21 -0400

    get scale factor from user, validate form

--- client/src/app/components/image-form/image-form.component.ts
+++ client/src/app/components/image-form/image-form.component.ts
@@ -13,6 +13,9 @@ export class ImageFormComponent {
   imageForm: FormGroup;
   file: any;
   responseImage: any;
+  checked_2_factor: boolean = false;
+  checked_4_factor: boolean = false;
+  scale_factor: any;
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.imageForm = this.fb.group({
       user_image: ['', Validators.required],
@@ -20,15 +23,32 @@ export class ImageFormComponent {
     });
   }
   validateForm(){
-    return true;
+    if(this.checked_2_factor === true && this.checked_4_factor === true){
+      this.scale_factor = 0;
+     return window.alert('You need to select only one value')
+    }
+    else if(this.checked_2_factor === false && this.checked_4_factor === false){
+      this.scale_factor = 0;
+      return window.alert('You need to select a value');
+    }
+    this.checked_2_factor === true ? this.scale_factor = 2: this.scale_factor = 4
+    this.onSubmit()
   }
   onFileChange(event: any) {
       this.file = event.target.files[0] as File; 
   }
+  onCheckChange_2_factor(){
+  this.checked_2_factor === true ? this.checked_2_factor = false : this.checked_2_factor = true;
+  console.log(this.checked_2_factor);
+  }
+  onCheckChange_4_factor(){
+    this.checked_4_factor === true ? this.checked_4_factor = false : this.checked_4_factor = true;
+    console.log(this.checked_4_factor);
+    }
   onSubmit() {
     const formData = new FormData();
     formData.append('user_image', this.file);
-    this.http.post('http://localhost:8000/scale?scaling=2', formData).subscribe((response: any) => {
+    this.http.post(`http://localhost:8000/scale?scaling=${this.scale_factor}`, formData).subscribe((response: any) => {
       if(response){
         const path = response.path;
         this.http.get<Blob>('http://localhost:8000/image/'+path, { headers: new HttpHeaders({ 'Content-Type': 'image/png' }), responseType: 'blob' as 'json' }).subscribe((response: any) => {
